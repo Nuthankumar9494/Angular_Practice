9e1b81cfb93887c885c3fd0f8838d2ed9706488f
NuthanKumar9494 <nuthankumar.gatadi@prasthana.com>
2022-07-13 15:27:30 +0530

    loops and nested loops in angular

--- src/app/propertybind/propertybind.component.ts
+++ src/app/propertybind/propertybind.component.ts
@@ -17,4 +17,17 @@ export class PropertybindComponent implements OnInit {
   marks='100';
   percentage=50;
   color='silver';
+  employee=['Nuthan','prahasith','pavan','rohit','pavani'];
+  students=['Ram','Ramu','reshna']
+  Employeesheet=[
+    {Name:'Nuthan',emaiid:'gatadi.xyz@.com',PhoneNo:89478924},
+    {Name:'bhasker',emaiid:'gatadi.xyz@.com',PhoneNo:89478924},
+    {Name:'babu',emaiid:'gatadi.xyz@.com',PhoneNo:89478924}
+  ]
+  Employeedata=[
+    {Name:'Nuthan',emaiid:'gatadi.xyz@.com',PhoneNo:89478924 ,Procjects:['Tea time','Yoga registration']},
+    {Name:'bhasker',emaiid:'gatadi.xyz@.com',PhoneNo:89478924,Procjects:['Tea world','Yoga registration']},
+    {Name:'babu',emaiid:'gatadi.xyz@.com',PhoneNo:89478924,Procjects:['Tea must','employe registration']}
+  ]
+
 }
